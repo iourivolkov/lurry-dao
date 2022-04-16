92f748f12057d117a121308b5f6b777aea8ed049
Iouri Volkov <volk.iouri@gmail.com>
2022-04-15 23:11:12 -0400

    Add logic for cases when user hasn't previously connected their wallet and when user has previously connected their wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useAddress, useMetamask } from "@thirdweb-dev/react";
 
 const App = () => {
+  // use hooks provided by third-web sdk
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address:", address);
+
+  // case 1 - user hasn't connected their wallet - call connectWallet
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to LurryDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect Wallet
+        </button>
+      </div>
+    );
+  }
+
+  // case 2 - have user's address - means user connected wallet to our site
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet Connected!</h1>
     </div>
   );
 };
